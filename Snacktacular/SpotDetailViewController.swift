53ef8be8e87c07fddb48fb233477d4563388f700
johnmekus <mekus@bc.edu>
2021-11-01 14:46:30 -0400

    8.5

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -13,12 +13,31 @@ class SpotDetailViewController: UIViewController
     @IBOutlet weak var addressTextField: UITextField!
     @IBOutlet weak var ratingLabel: UILabel!
     
+    var spot: Spot!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        if spot == nil
+        {
+            spot = Spot()
+        }
+        updateUserInterface()
     }
     
-    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem)
+    func updateUserInterface()
+    {
+        nameTextField.text = spot.name
+        addressTextField.text = spot.address
+    }
+    
+    func updateFromInterface()
+    {
+        spot.name = nameTextField.text!
+        spot.address = addressTextField.text!
+    }
+    
+    func leaveViewController()
     {
         let isPresentingInAddMode = presentingViewController is UINavigationController
         if isPresentingInAddMode
@@ -29,6 +48,28 @@ class SpotDetailViewController: UIViewController
         {
             navigationController?.popViewController(animated: true)
         }
+    }
+    
+    
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem)
+    {
+        updateFromInterface()
+        spot.saveData
+        { success in
+            if success
+            {
+                self.leaveViewController()
+            }
+            else
+            {
+                self.oneButtonAlert(title: "Save Failed", message: "For some reason, the data would not save to the cloud.")
+            }
+        }
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem)
+    {
+        leaveViewController()
     }
     
 }
