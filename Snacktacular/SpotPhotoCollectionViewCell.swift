e1f928d8a098ea942d1717411750cc676f6ffc5d
johnmekus <mekus@bc.edu>
2021-11-15 14:13:23 -0500

    8.24

--- Snacktacular/SpotPhotoCollectionViewCell.swift
+++ Snacktacular/SpotPhotoCollectionViewCell.swift
@@ -6,22 +6,29 @@
 //
 
 import UIKit
+import SDWebImage
 
 class SpotPhotoCollectionViewCell: UICollectionViewCell
 {
     @IBOutlet weak var photoImageView: UIImageView!
     
     var spot: Spot!
-    var photo: Photo! {
-        didSet {
-            photo.loadImage(spot: spot) { (success) in
-                if success {
-                    self.photoImageView.image = self.photo.image
-                } else {
-                    print("ERROR: no success in loading photo in SpotPhotoCollectionViewCell")
+    var photo: Photo!
+    {
+        didSet
+        {
+            if let url = URL(string: self.photo.photoURL) {
+                self.photoImageView.sd_imageTransition = .fade
+                self.photoImageView.sd_imageTransition?.duration = 0.2
+                self.photoImageView.sd_setImage(with: url)
+            } else {
+                print("ERROR: URL didn't work \(self.photo.photoURL)")
+                self.photo.loadImage(spot: self.spot) { success in
+                    self.photo.saveData(spot: self.spot) { success in
+                        print("Image updated with URL \(self.photo.photoURL)")
+                    }
                 }
             }
-
         }
     }
 }
