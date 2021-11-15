b1392f7d1e398f70147e2be86306578ef3847beb
johnmekus <mekus@bc.edu>
2021-11-15 11:15:08 -0500

    8.22

--- Snacktacular/PhotoViewController.swift
+++ Snacktacular/PhotoViewController.swift
@@ -74,6 +74,11 @@ class PhotoViewController: UIViewController
         }
     }
     
+    func updateFromUserInterface() {
+        photo.description = descriptionTextView.text!
+        photo.image = photoImageView.image!
+    }
+    
     func addBordersToEditableObjects() {
           descriptionTextView.addBorder(width: 0.5, radius: 5.0, color: .black)
       }
@@ -96,5 +101,14 @@ class PhotoViewController: UIViewController
     
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
         leaveViewController()
+        
+        updateFromUserInterface()
+        photo.saveData(spot: spot) { (success) in
+            if success {
+                self.leaveViewController()
+            } else  {
+                print("ERROR: Can't unwind segue from photoviewcontroller because of photo saving error.")
+            }
+        }
     }
 }
